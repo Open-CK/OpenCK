979d4d81c04356339a1e91c8cf123fba984c3483
Alex Pawelko <hisaysajp@gmail.com>
2017-07-25 10:13:14 -0600

    Update generate_docs.bat

--- OpenCK/doc/generate_docs.bat
+++ OpenCK/doc/generate_docs.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 REM This will only worked with doxygen installed.
 REM Install it here: http://www.stack.nl/~dimitri/doxygen/
-doxygen doxyfile
+WHERE doxygen doxyfile
+IF %ERRORLEVEL% NEQ 0 ECHO Doxygen not found!
 pause
