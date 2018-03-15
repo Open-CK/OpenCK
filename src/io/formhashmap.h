3aca466387c95a8cff02e1879af13fcfdab892dc
Malrama <clari.skyrim63@gmail.com>
2018-03-15 01:14:33 +0100

    Fixed the name to the actuall type.

--- src/io/formhashmap.h
+++ src/io/formhashmap.h
@@ -9,15 +9,15 @@ namespace io
     static QHash<quint32, QString> FormHashIndex
     {
         //Add all available Record Types that are supported. Please make sure they are sorted alphabetically for a better overview.
-        {'AACT', "AACT"},
-        {'CLAS', "CLAS"},
-        {'GLOB', "GLOB"},
-        {'GMST', "GMST"},
-        {'GRUP', "GRUP"},
-        {'KYWD', "KYWD"},
-        {'LCRT', "LCRT"},
-        {'TES4', "TES4"},
-        {'TXST', "TXST"},
+        {'AACT', "Action"},
+        {'CLAS', "Class"},
+        {'GLOB', "Global Variable"},
+        {'GMST', "Game Setting"},
+        {'GRUP', "Form Group"},
+        {'KYWD', "Keyword"},
+        {'LCRT', "Location Reference Type"},
+        {'TES4', "Plugin info"},
+        {'TXST', "Texture Set"},
     };
 }
 
